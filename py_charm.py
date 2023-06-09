03acf8a0be4b6e21b006c41cfd1ecb73ad74ee6e
KostyaLysenko <kostyalysenko6@gmail.com>
2023-06-09 17:58:53 +0300

    Commit 006 py_charm

---
+++ py_charm.py
@@ -0,0 +1,16 @@
+def mane_function(**kwargs):
+    sum = ""
+    for k, v in kwargs.items():
+        sum += v
+    print(sum)
+mane_function(aa="1", d="2", a="ertyh", bb= "dgdfshdsfh")
+##
+#comment
+########
+#ще декілька рядків коду в master
+#############
+a=10
+b=2
+test_function = a//b
+print(test_function)
+#Ще декілька рядків коду для test
